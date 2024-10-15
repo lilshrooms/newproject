5fb606d055398bae87c87f229abf1f3c8ea8ba29
lilshrooms <121637638+lilshrooms@users.noreply.github.com>
2024-10-15 13:37:12 -0400

    fixing deployment

--- next-app2/next.config.mjs
+++ next-app2/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['ibktlbeduwmyvnvoubha.supabase.co'], // Add Supabase domain if you're loading images from there
+  },
+};
 
 export default nextConfig;
